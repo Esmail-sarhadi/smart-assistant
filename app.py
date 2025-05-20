d76a1ab96231e1251cb948e6aca1e7c34ba08686
Esmail Sarhadi <45729949+Esmail-sarhadi@users.noreply.github.com>
2025-05-20 22:04:25 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import sqlite3
-from datetime import datetime
+from datetime import datetime 
 
 app = Flask(__name__)  
 app.static_folder = 'static'
